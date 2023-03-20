cc7bb450c416f14af19d512c632ff4b84734de23
girishns <girishkrish1998yuv@gmail.com>
2023-03-20 21:37:17 +0530

    eda and model training

--- venv3/Library/bin/c_rehash.pl
+++ venv3/Library/bin/c_rehash.pl
@@ -13,7 +13,7 @@
 # and add symbolic links to their hash values.
 
 my $dir = "C:\\Program Files\\Common Files\\ssl";
-my $prefix = "E:/Generic/venv3\\Library";
+my $prefix = "e:/Generic/venv3\\Library";
 
 my $errorcount = 0;
 my $openssl = $ENV{OPENSSL} || "openssl";
