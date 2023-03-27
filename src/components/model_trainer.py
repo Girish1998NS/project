2948b9603c4cecdb0a084ffa925b1f220da66e08
girishns <girishkrish1998yuv@gmail.com>
2023-03-27 21:55:07 +0530

    predict pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -37,14 +37,31 @@ class ModelTrainer:
                 test_array[:,-1]
                 )
             models={"random forest":RandomForestRegressor(),
-                    "Decision tress":DecisionTreeRegressor(),
+                    "Decision tree":DecisionTreeRegressor(),
                     "Gradient boosting":GradientBoostingRegressor(),
                     "Linear regression":LinearRegression(),
                     "K-neighbhours":KNeighborsRegressor(),
                     "Catboosting":CatBoostRegressor(verbose=False),
                     "addaboosting":AdaBoostRegressor(),
                     }
-            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            params={
+                "Decison tree":{
+                'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson']},
+                "random forest":{'n_estimators': [8,16,32,64,128,256]},
+                "Gradient Boosting":{ 'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9], 'n_estimators': [8,16,32,64,128,256]},
+                "Linear Regression":{},
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]},
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]}
+            }
+            
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
             #To get best model from score
             best_model_score=max(sorted(model_report.values()))
             #to get best model name
